1510bacb6a469b76c00ae791934708ce1882eaad
Ben Swannack <ben.swannack@softwire.com>
2020-11-04 21:32:19 +0000

    improved colours and positions

--- firework.js
+++ firework.js
@@ -31,46 +31,32 @@ Firework.prototype.explodeIfTime = function () {
       const iv = p5.Vector.fromAngle(radians(angle * i), 0.5);
       this.debris.push(new Particle(ip, iv, this.explosionColor));
     }
-    console.log(this.debris);
   } else {
     this.counter++;
   }
 };
 
 function randomPrimaryColor() {
-  const col = 200;
-  let r = 0;
-  let g = 0;
-  let b = 0;
-  switch (Math.floor(Math.random() * 3)) {
-    case 0:
-      r = col;
-      g = 0;
-      b = 0;
-      break;
-    case 1:
-      r = 0;
-      g = col;
-      b = 0;
-      break;
-    case 2:
-      r = 0;
-      g = 100;
-      b = col;
-      break;
-  }
+  fireworkColors = [
+      color(176, 28, 217),
+      color(176, 28, 217),
+      color(255, 53, 31),
+      color(255, 195, 31),
+      color(89, 209, 25),
+      color(25, 117, 209),
+  ];
 
-  return color(r, g, b)
+  return fireworkColors[randomIntBetween(0, fireworkColors.length)]
 }
 
 function randomStartPosition() {
-  const x = 60 * randomSigned();
+  const x = 50 * randomSigned();
   return createVector(x, 0)
 }
 
 function randomStartVelocity() {
-  const xv = 0.3 * randomSigned();
-  const yv = 2 + 0.5 * randomSigned();
+  const xv = 0.8 * randomSigned();
+  const yv = 2 + 0.7 * randomSigned();
   return createVector(xv, yv)
 }
 
